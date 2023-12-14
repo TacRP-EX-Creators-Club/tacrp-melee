e05f0089f0a2988ebbe116608c0808755cf078c9
TheOnly8Z <888888zombies@gmail.com>
2023-12-13 17:10:53 -0800

    bat and pan wip melee rework

--- lua/weapons/tacrp_m_bat.lua
+++ lua/weapons/tacrp_m_bat.lua
@@ -1,4 +1,4 @@
-SWEP.Base = "tacrp_base"
+SWEP.Base = "tacrp_base_knife"
 SWEP.Spawnable = true
 
 AddCSLuaFile()
@@ -18,68 +18,28 @@ SWEP.Credits = "Model & Texture: Yogensia"
 SWEP.ViewModel = "models/weapons/tacint_melee/v_bat.mdl"
 SWEP.WorldModel = "models/weapons/tacint_melee/w_bat.mdl"
 
-SWEP.NoRanger = true
-SWEP.NoStatBox = true
-
 SWEP.Slot = 0
-
-SWEP.NPCUsable = false
-
-// misc. shooting
-
-SWEP.PrimaryMelee = true
 
 SWEP.MeleeDamage = 40
 SWEP.MeleeAttackTime = 0.5
 SWEP.MeleeRange = 128
-SWEP.MeleeAttackMissTime = 0.65
-
-SWEP.Melee2Damage = 65
-SWEP.Melee2AttackTime = 0.7
-SWEP.Melee2Range = 96
-SWEP.Melee2AttackMissTime = 0.9
+SWEP.MeleeAttackMissTime = 0.6
 
 SWEP.MeleeDamageType = DMG_CLUB
 
-SWEP.MeleeThrowTime = 1
-SWEP.MeleeThrowTimeWait = 0.2
+SWEP.MeleeThrowForce = 1200
 
-SWEP.Firemode = 0
-
-SWEP.RPM = 110
-
-SWEP.CanBlindFire = false
-
-SWEP.Ammo = ""
-SWEP.ClipSize = -1
-SWEP.Primary.ClipSize = -1
-
-// handling
-
-SWEP.MoveSpeedMult = 1
-
-SWEP.MeleeSpeedMult = 1
-SWEP.MeleeSpeedMultTime = 0.5
-
-SWEP.SprintToFireTime = 0.25
-
-SWEP.QuickNadeTimeMult = 0.8
-
-SWEP.Scope = false
-
-SWEP.Sway = 0
+SWEP.MeleePerkStr = 0.2
+SWEP.MeleePerkAgi = 0.5
+SWEP.MeleePerkInt = 0.8
 
 // hold types
 
 SWEP.HoldType = "melee"
-SWEP.HoldTypeSprint = "knife"
+SWEP.HoldTypeSprint = "melee"
 
-SWEP.GestureShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_PISTOL
-SWEP.GestureReload = ACT_HL2MP_GESTURE_RELOAD_PISTOL
 SWEP.GestureBash = ACT_HL2MP_GESTURE_RANGE_ATTACK_MELEE
 SWEP.GestureBash2 = ACT_HL2MP_GESTURE_RANGE_ATTACK_MELEE
-
-SWEP.MidAirSpreadPenalty = 0
 
 SWEP.PassiveAng = Angle(-7.5, 0, 5)
 SWEP.PassivePos = Vector(3, 0, -10)
@@ -94,11 +54,6 @@ SWEP.SprintMidPoint = {
     Pos = Vector(2, 0, -5),
     Ang = Angle(0, 0, 0)
 }
-
-SWEP.HolsterVisible = false
-SWEP.HolsterSlot = TacRP.HOLSTER_SLOT_GEAR
-SWEP.HolsterPos = Vector(2, 0, 0)
-SWEP.HolsterAng = Angle(-90, -90, 15)
 
 // sounds
 
@@ -115,57 +70,6 @@ SWEP.Sound_MeleeHit = path .. "bat_hit.wav"
 
 SWEP.Sound_MeleeHitBody = path .. "bat_hit.wav"
 
-
 SWEP.Sound_MeleeSwing = {
-	"weapons/iceaxe/iceaxe_swing1.wav"
+    "weapons/iceaxe/iceaxe_swing1.wav"
 }
-
-// attachments
-
-SWEP.Attachments = {
-    [1] = {
-        PrintName = "Technique",
-        Category = "melee_tech",
-        AttachSound = "TacRP/weapons/flashlight_on.wav",
-        DetachSound = "TacRP/weapons/flashlight_off.wav",
-    },
-    [2] = {
-        PrintName = "Special",
-        Category = "melee_spec",
-        AttachSound = "TacRP/weapons/flashlight_on.wav",
-        DetachSound = "TacRP/weapons/flashlight_off.wav",
-    },
-}
-
-SWEP.FreeAim = false
-
-SWEP.DrawCrosshair = true
-SWEP.DrawCrosshairInSprint = true
-SWEP.CrosshairStatic = true
-
-local function addsound(name, spath)
-    sound.Add({
-        name = name,
-        channel = 16,
-        volume = 1.0,
-        sound = spath
-    })
-end
-
-addsound("tacint_knife2.deploy", "tacrp/magtap.ogg")
-
-function SWEP:PrimaryAttack()
-    local stop = self:RunHook("Hook_PreShoot")
-    if stop then return end
-
-    self:Melee()
-    return
-end
-
-function SWEP:ThinkSprint()
-end
-
-function SWEP:ThinkSights()
-end
-
-SWEP.AutoSpawnable = false
--- lua/weapons/tacrp_m_pan.lua
+++ lua/weapons/tacrp_m_pan.lua
@@ -1,4 +1,4 @@
-SWEP.Base = "tacrp_base"
+SWEP.Base = "tacrp_base_knife"
 SWEP.Spawnable = true
 
 AddCSLuaFile()
@@ -18,7 +18,7 @@ SWEP.ViewModel = "models/weapons/tacint_melee/v_pansexual.mdl"
 SWEP.WorldModel = "models/weapons/tacint_melee/w_pansexual.mdl"
 
 SWEP.NoRanger = true
-SWEP.NoStatBox = true
+SWEP.NoStatBox = false
 
 SWEP.Slot = 0
 
@@ -33,52 +33,21 @@ SWEP.MeleeAttackTime = 0.5
 SWEP.MeleeRange = 128
 SWEP.MeleeAttackMissTime = 0.65
 
-SWEP.Melee2Damage = 65
-SWEP.Melee2AttackTime = 0.7
-SWEP.Melee2Range = 96
-SWEP.Melee2AttackMissTime = 0.9
-
 SWEP.MeleeDamageType = DMG_CLUB
 
-SWEP.MeleeThrowTime = 1
-SWEP.MeleeThrowTimeWait = 0.2
+SWEP.MeleeThrowForce = 1000
 
-SWEP.Firemode = 0
-
-SWEP.RPM = 110
-
-SWEP.CanBlindFire = false
-
-SWEP.Ammo = ""
-SWEP.ClipSize = -1
-SWEP.Primary.ClipSize = -1
-
-// handling
-
-SWEP.MoveSpeedMult = 1
-
-SWEP.MeleeSpeedMult = 1
-SWEP.MeleeSpeedMultTime = 0.5
-
-SWEP.SprintToFireTime = 0.25
-
-SWEP.QuickNadeTimeMult = 0.8
-
-SWEP.Scope = false
-
-SWEP.Sway = 0
+SWEP.MeleePerkStr = 0.8
+SWEP.MeleePerkAgi = 0.4
+SWEP.MeleePerkInt = 0.3
 
 // hold types
 
 SWEP.HoldType = "melee"
 SWEP.HoldTypeSprint = "knife"
 
-SWEP.GestureShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_PISTOL
-SWEP.GestureReload = ACT_HL2MP_GESTURE_RELOAD_PISTOL
 SWEP.GestureBash = ACT_HL2MP_GESTURE_RANGE_ATTACK_MELEE
 SWEP.GestureBash2 = ACT_HL2MP_GESTURE_RANGE_ATTACK_MELEE
-
-SWEP.MidAirSpreadPenalty = 0
 
 SWEP.PassiveAng = Angle(-5, -2, 0)
 SWEP.PassivePos = Vector(1, 0, -5)
@@ -111,16 +80,16 @@ SWEP.AnimationTranslationTable = {
 }
 
 SWEP.Sound_MeleeHit = {
-	path1 .. "pan_impact_world1.wav",
-	path1 .. "pan_impact_world2.wav",
-	path1 .. "pan_impact_world3.wav",
+    path1 .. "pan_impact_world1.wav",
+    path1 .. "pan_impact_world2.wav",
+    path1 .. "pan_impact_world3.wav",
 }
 
 SWEP.Sound_MeleeHitBody = {
-	path1 .. "melee_frying_pan_01.wav",
-	path1 .. "melee_frying_pan_02.wav",
-	path1 .. "melee_frying_pan_03.wav",
-	path1 .. "melee_frying_pan_04.wav",
+    path1 .. "melee_frying_pan_01.wav",
+    path1 .. "melee_frying_pan_02.wav",
+    path1 .. "melee_frying_pan_03.wav",
+    path1 .. "melee_frying_pan_04.wav",
 }
 
 SWEP.Sound_MeleeSwing = "weapons/iceaxe/iceaxe_swing1.wav"
@@ -141,36 +110,3 @@ SWEP.Attachments = {
         DetachSound = "TacRP/weapons/flashlight_off.wav",
     },
 }
-
-SWEP.FreeAim = false
-
-SWEP.DrawCrosshair = true
-SWEP.DrawCrosshairInSprint = true
-SWEP.CrosshairStatic = true
-
-local function addsound(name, spath)
-    sound.Add({
-        name = name,
-        channel = 16,
-        volume = 1.0,
-        sound = spath
-    })
-end
-
-addsound("tacint_knife2.deploy", "tacrp/magtap.ogg")
-
-function SWEP:PrimaryAttack()
-    local stop = self:RunHook("Hook_PreShoot")
-    if stop then return end
-
-    self:Melee()
-    return
-end
-
-function SWEP:ThinkSprint()
-end
-
-function SWEP:ThinkSights()
-end
-
-SWEP.AutoSpawnable = false
